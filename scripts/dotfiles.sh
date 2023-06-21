ec2992b63d1aed6fc42188ec5b1eaa7c1a6ab77f
Mark Devenyi <markdevenyidev@gmail.com>
2023-06-21 21:38:03 +0200

    dotfiles.sh: Fix wrapper error message

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -10,14 +10,12 @@ BOLD="\033[1m"
 ENDCOLOR="\e[0m"
 
 wrapper() {
-    "${@:2}"
-    ret=$?
-    if [[ $ret -eq 0 ]]
+    output=$(${@:2} 2>&1)
+    if [[ $? -eq 0 ]]
     then
         printf "${BOLD}[${GREEN} Ok ${ENDCOLOR}${BOLD}] $1${ENDCOLOR}\n"
     else
-        printf "${BOLD}[${RED}Fail${ENDCOLOR}${BOLD}] $1${ENDCOLOR}\nError: $ret\n"
-        exit $ret
+        printf "${BOLD}[${RED}Fail${ENDCOLOR}${BOLD}] $1${ENDCOLOR}\n       ${BOLD}${RED}Error${ENDCOLOR} $ret\n"
     fi
 }
 
