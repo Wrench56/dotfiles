603bae7d32335b9f7805ef9ba7329afdf0a397d9
Mark Devenyi <markdevenyidev@gmail.com>
2023-07-06 02:00:00 +0200

    Fix dunst.sh

--- scripts/dunst.sh
+++ scripts/dunst.sh
@@ -6,23 +6,24 @@ USERNAME=$(whoami)
 DOTFILES=$(dirname -- "$(realpath -- "$(dirname $(realpath -s $0))")")
 
 # Replace @USER with the current username
-cd $DOTFILES/dunst/script
+cd $DOTFILES/dunst/scripts
 for file in *; do sed -i "s/@USER/${USERNAME}/" $file; done
 cd $DOTFILES/dunst/other
 for file in *; do sed -i "s/@USER/${USERNAME}/" $file; done
 
 # Link dunst/scripts directory to ~/.local/bin/dunst
-if [ -d "~/.local/bin/dunst" ] then; rm -rf ~/.local/bin/dunst; fi
+if [ -d ~/.local/bin/dunst ]; then rm -rf ~/.local/bin/dunst; fi
 ln -s $DOTFILES/dunst/script ~/.local/bin/dunst
 
 # Link rules
-ln -s $DOTFILES/dunst/other/power.rules /etc/udev/rules.d/power.rules
+if [ -f /etc/udev/rules.d/power.rules ]; then sudo rm /etc/udev/rules.d/power.rules; fi
+sudo ln -s $DOTFILES/dunst/other/power.rules /etc/udev/rules.d/power.rules
 
 # Link systemd files
 for file in *
 do
-    if [ -d "~/.config/systemd/user/$file" ] then; rm ~/.config/systemd/user/$file; fi
-    ln -s $file ~/.config/systemd/user/$file
+    if [ -f ~/.config/systemd/user/$file ]; then rm ~/.config/systemd/user/$file; fi
+    cp $file ~/.config/systemd/user/$file
 done
 
 # Enable user specific systemd services
