db37063f86e8c21e0e78a2e8becd03d996922c38
Wrench56 <dmarkreg@gmail.com>
2023-07-11 20:05:23 +0200

    Fix conky.sh script

--- scripts/conky.sh
+++ scripts/conky.sh
@@ -8,14 +8,14 @@
 
 sudo pacman -S --noconfirm --needed networkmanager
 
-NETIF=$(nmcli | grep -P -o "wlp.*)?=: connected)")
+NETIF=$(nmcli | grep -P -o "wlp.*(?=: connected)")
 CPUS=$(cat /proc/cpuinfo | grep -P -o -m1 "(?<=(siblings\t: )).*")
 
 # Set network interface
 sed -i "s/@NETIF/${NETIF}/g" ~/.conkyrc
 
 # Set CPU cores
-for i in $(eval echo "{1..$VAR}")
+for i in $(eval echo "{1..$CPUS}")
 do
     REAL_NUM=$(($i - 1))
     sed -i "s/@CPU_CORES/\${color1}\${goto 35}Core ${i}: \${color}\${freq_g ${i}}GHz \${alignr}\${cpu cpu${REAL_NUM}}% \${cpubar cpu${REAL_NUM} 4, 100}\n@CPU_CORES/" ~/.conkyrc
