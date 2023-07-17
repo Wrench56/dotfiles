f64f9ab4031ee8f008a2d09435f293976ff7cd60
Mark Devenyi <markdevenyidev@gmail.com>
2023-07-17 16:27:29 +0200

    Fix minor issues in maintenance.sh

--- scripts/maintenance.sh
+++ scripts/maintenance.sh
@@ -2,8 +2,10 @@
 
 # Maintenance script for arch
 
+# shellcheck disable=SC2312
+
 printf "\033[1m[\e[33mWARN\e[0m\033[1m] This script will clear the terminal multiple times!\e[0m\n"
-read -p "       Press Enter to continue" </dev/tty
+read -r -p "       Press Enter to continue" </dev/tty
 
 # Update system
 sudo pacman -Syu --noconfirm
@@ -14,13 +16,13 @@ clear
 
 printf "Showing systemctl errors\n"
 sudo systemctl --failed
-read -p "Press Enter to continue" </dev/tty
+read -r -p "Press Enter to continue" </dev/tty
 clear
 
 # Show journal errors
 printf "Showing journal errors\n"
 sudo journalctl -p 3 -xb
-read -p "Press Enter to continue" </dev/tty
+read -r -p "Press Enter to continue" </dev/tty
 clear
 
 # Show the size of the .cache & journal directory and others
@@ -28,7 +30,7 @@ printf "Showing important directory sizes\n"
 du -sh ~/.cache/
 du -sh /var/log/journal/
 du -sh ~/.config
-read -p "Press Enter to delete the logs & clear the cache" </dev/tty
+read -r -p "Press Enter to delete the logs & clear the cache" </dev/tty
 clear
 
 # Delete the journal logs up until 2 weeks
