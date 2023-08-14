920d89af2c229fb19b3d0941f5a4222224ea9f7f
Wrench56 <dmarkreg@gmail.com>
2023-08-14 05:19:25 +0200

    setup.sh: Install onefetch

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,7 +12,7 @@ sudo pacman -Sy --noconfirm pacman-contrib
 sudo systemctl enable paccache.timer
 
 # Get some frequently used packages
-sudo pacman -S --noconfirm neofetch htop git nano man-db exa wget bc
+sudo pacman -S --noconfirm neofetch onefetch htop git nano man-db exa wget bc
 
 # Get yay AUR package helper
 sudo pacman -S --needed --noconfirm git base-devel
