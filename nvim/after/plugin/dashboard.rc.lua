afef10c04968de8c266562c2ef616004a3eee0d6
Wrench56 <dmarkreg@gmail.com>
2023-08-18 23:52:02 -0400

    Update dashboard.nvim configuration

--- nvim/after/plugin/dashboard.rc.lua
+++ nvim/after/plugin/dashboard.rc.lua
@@ -1,26 +1,32 @@
 local status, dashboard = pcall(require, 'dashboard')
 if (not status) then return end
 
-version_table = vim.version()
-version = tostring(version_table['major']) .. '.' .. tostring(version_table['minor']) .. '.' .. tostring(version_table['patch'])
+local version_table = vim.version()
+local version = tostring(version_table['major']) .. '.' .. tostring(version_table['minor']) .. '.' .. tostring(version_table['patch'])
 
 dashboard.setup({
    config = {
       week_header = {
          enable = true,
-         concat = ' - Neovim v' .. version
+         concat = '- Neovim v' .. version
       },
       disable_move = true,
       shortcut = {
          {
-            desc = '󰚰 Update',
+            desc = ' Update',
             group = '@property',
             action = 'Lazy update',
-            key = 'u' 
+            key = 'u'
+         },
+         {
+            desc = '󰚰 Mason',
+            group = 'Directory',
+            action = 'Mason',
+            key = 'm'
          },
          {
             desc = ' Files',
-            group = 'Label',
+            group = 'LineNr',
             action = 'Telescope find_files',
             key = 'f',
          },
@@ -29,7 +35,7 @@ dashboard.setup({
             group = 'DiagnosticHint',
             action = 'checkhealth',
             key = 'h',
-         },
+         }
       },
       footer = {
          '',
